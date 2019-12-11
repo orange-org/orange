144f6ea7fb24a964604c5e11f4fbf186e183ce40
M.K. Safi <msafi@msafi.com>
2019-12-10 18:38:15 -0800

    Initial implementation of `prepare-dist.ts`

--- build-scripts/prepare-dist.ts
+++ build-scripts/prepare-dist.ts
@@ -1,12 +1,30 @@
-// import * as ncp from 'ncp';
 import * as rimraf from "rimraf";
 import * as pkgDir from "pkg-dir";
 import * as path from "path";
+import { ncp } from "ncp";
 
 const rootPath = pkgDir.sync() as string;
+const destination = path.join(rootPath, "dist");
+const source = path.join(rootPath, "src");
 
-//
-// rimraf(path.join(rootPath, 'dist'));
+rimraf(destination, () => null);
+
+ncp(
+  source,
+  destination,
+  {
+    filter: fileName => {
+      return [".ts", ".tsx"].includes(path.extname(fileName)) === false;
+    }
+  },
+  error => {
+    if (error) {
+      return console.error(error);
+    }
+
+    console.log("Done!");
+  }
+);
 
 // Remove dist folder
 // Recreate dist folder

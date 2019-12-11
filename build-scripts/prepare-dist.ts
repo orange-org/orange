e0cabb560069bd7f636a30359ae748c0f55bb48f
M.K. Safi <msafi@msafi.com>
2019-12-10 18:12:01 -0800

    Install and configure ESLint

---
+++ build-scripts/prepare-dist.ts
@@ -0,0 +1,13 @@
+// import * as ncp from 'ncp';
+import * as rimraf from 'rimraf';
+import * as pkgDir from 'pkg-dir';
+import * as path from 'path';
+
+const rootPath = pkgDir.sync() as string;
+
+//
+rimraf(path.join(rootPath, 'dist'));
+
+// Remove dist folder
+// Recreate dist folder
+// Copy all non ts/tsx files to dist folder

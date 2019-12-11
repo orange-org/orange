cbe90a330189ae847e7451c9c9832edb68dc1f62
M.K. Safi <msafi@msafi.com>
2019-12-10 19:14:17 -0800

    Finish prepare-dist

--- build-scripts/prepare-dist.ts
+++ build-scripts/prepare-dist.ts
@@ -1,4 +1,3 @@
-import * as rimraf from "rimraf";
 import * as pkgDir from "pkg-dir";
 import * as path from "path";
 import * as fs from "fs-extra";
@@ -8,16 +7,12 @@ const rootPath = pkgDir.sync() as string;
 const destination = path.join(rootPath, "dist");
 const source = path.join(rootPath, "src");
 
-rimraf(destination, () => null);
-
 bluebird
   .try(async () => {
+    await fs.remove(destination);
     await fs.mkdirp(destination);
     await fs.copy(source, destination, {
       filter: fileName => {
-        console.log("=\nFILE: prepare-dist.ts\nLINE: 17\n=");
-        console.log("fileName", fileName);
-        console.log("path.extname(fileName)", path.extname(fileName));
         return [".ts", ".tsx"].includes(path.extname(fileName)) === false;
       }
     });
@@ -26,7 +21,3 @@ bluebird
     console.error("Error while running `prepare-dist`:", error.message);
     process.exit(1);
   });
-
-// Remove dist folder
-// Recreate dist folder
-// Copy all non ts/tsx files to dist folder

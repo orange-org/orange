5dadcb06b841caa398475518b334a707651c4986
M.K. Safi <msafi@msafi.com>
2020-03-30 12:43:29 -0700

    Check-in .nonce file

--- scripts/generate-nonce.sh
+++ scripts/generate-nonce.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-NONCE_FILE=`dirname $0`/../.nonce
-
-if test -f "$NONCE_FILE"; then
-    echo "nonce file exist"
-    exit 0
-fi
-
 echo "Creating nonce file at $NONCE_FILE"
 
 NONCE=$(node -e console.log\(crypto.randomBytes\(18\).toString\(\"base64\"\)\))

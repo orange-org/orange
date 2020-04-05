0f337b750115239063ac727cadbf3884243ea194
M.K. Safi <msafi@msafi.com>
2020-04-04 19:33:15 -0700

    Remove depcheck

--- scripts/createPackage.js
+++ scripts/createPackage.js
@@ -3,7 +3,6 @@ const packager = require("electron-packager");
 const { resolve } = require("path");
 const bluebird = require("bluebird");
 const shelljs = require("shelljs");
-const Listr = require("listr");
 const fs = require("fs-extra");
 const archiver = require("archiver");
 
@@ -15,57 +14,43 @@ const electronPackagerArtifactsDir = resolve(artifactsDir, "electronPackager");
 shelljs.cd(rootPath);
 
 async function bundleElectronApp() {
-  const tasks = new Listr([
-    {
-      title: "Building source code",
-      task: async () => {
-        await new Promise(resolve_ =>
-          shelljs.exec("npm run build", { silent: true }, resolve_),
-        );
-      },
-    },
+  console.log("Building source code...");
+  await new Promise(resolve_ =>
+    shelljs.exec("npm run build", { silent: true }, resolve_),
+  );
 
-    {
-      title: "Creating Electron package",
-      task: async () => {
-        await packager({
-          arch: "x64",
-          dir: webpackArtifactsDir,
-          out: electronPackagerArtifactsDir,
-          icon: resolve(rootPath, "src/assets/orange"),
-          overwrite: true,
-          platform: "darwin",
-          prune: false,
-          quiet: true,
-          appVersion: process.env.TRAVIS_TAG,
-        });
-      },
-    },
+  console.log("Creating Electron package...");
+  await packager({
+    arch: "x64",
+    dir: webpackArtifactsDir,
+    out: electronPackagerArtifactsDir,
+    icon: resolve(rootPath, "src/assets/orange"),
+    overwrite: true,
+    platform: "darwin",
+    prune: false,
+    quiet: true,
+    appVersion: process.env.TRAVIS_TAG,
+  });
 
-    {
-      title: "Compressing Electron package",
-      task: async (_ctx, task) => {
-        await new Promise(resolve_ => {
-          const archive = archiver("zip", { zlib: { level: 9 } });
-          const output = fs.createWriteStream(
-            resolve(electronPackagerArtifactsDir, "Orange (macOS).zip"),
-          );
-          archive.pipe(output);
-          archive.directory(
-            resolve(electronPackagerArtifactsDir, "Orange-darwin-x64/"),
-            false,
-          );
-          output.on("close", resolve_);
-          archive.on("error", err => {
-            throw err;
-          });
-          archive.finalize();
-        });
-      },
-    },
-  ]);
+  console.log("Compressing Electron package...");
+  await new Promise(resolve_ => {
+    const archive = archiver("zip", { zlib: { level: 9 } });
+    const output = fs.createWriteStream(
+      resolve(electronPackagerArtifactsDir, "Orange (macOS).zip"),
+    );
+    archive.pipe(output);
+    archive.directory(
+      resolve(electronPackagerArtifactsDir, "Orange-darwin-x64/"),
+      false,
+    );
+    output.on("close", resolve_);
+    archive.on("error", err => {
+      throw err;
+    });
+    archive.finalize();
+  });
 
-  return tasks.run();
+  console.log("Done!");
 }
 
 bluebird.try(bundleElectronApp).catch(console.error);

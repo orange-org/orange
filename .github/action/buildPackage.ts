85a9d28656908e81c78657d8c70d8386a6111bf8
M.K. Safi <msafi@msafi.com>
2020-04-09 18:40:01 -0700

    Implement upload artifact

--- .github/action/buildPackage.ts
+++ .github/action/buildPackage.ts
@@ -1,21 +1,37 @@
 /* eslint-disable no-console */
 import * as core from "@actions/core";
-import bluebird from "bluebird";
+import * as artifact from "@actions/artifact";
+import fs from "fs-extra";
 import * as packager from "electron-packager";
+import * as archiver from "archiver";
 import { getAppVersion } from "./getAppVersion";
 import { execWithErrorMessage } from "./utils";
 
-const githubActionElectronPackagerPlatformNameMap = {
-  "macos-latest": "darwin",
-  "ubuntu-latest": "linux",
-  "windows-latest": "win32",
-} as any;
+const artifactClient = artifact.create();
+
+const appVersion = getAppVersion();
+
+const platformDefinitions = {
+  "macos-latest": {
+    electronPackagerPlatform: "darwin",
+    archiveName: `Orange-v${appVersion}-macOS.zip`,
+  },
+  "ubuntu-latest": {
+    electronPackagerPlatform: "linux",
+    archiveName: `Orange-v${appVersion}-Linux.zip`,
+  },
+  "windows-latest": {
+    electronPackagerPlatform: "win32",
+    archiveName: `Orange-v${appVersion}-Windows.zip`,
+  },
+};
 
 export async function buildPackage() {
   console.log("Building source code...");
   await execWithErrorMessage("npm run build", "`npm run build` failed");
 
   const os = core.getInput("os", { required: true });
+  const platformDefinition = platformDefinitions[os];
 
   console.log(`Creating Electron package on ${os}...`);
   await packager({
@@ -24,33 +40,34 @@ export async function buildPackage() {
     out: "artifacts/electronPackager",
     icon: "src/assets/orange",
     overwrite: true,
-    platform: githubActionElectronPackagerPlatformNameMap[os],
+    platform: platformDefinition.electronPackagerPlatform,
     prune: false,
     appVersion: getAppVersion(),
   });
 
-  // console.log("Compressing Electron package...");
-  // await new Promise(resolve_ => {
-  //   const archive = archiver("zip", { zlib: { level: 9 } });
-  //   const output = fs.createWriteStream(
-  //     resolve(
-  //       electronPackagerArtifactsDir,
-  //       `Orange-macOS-${process.env.TRAVIS_TAG}.zip`,
-  //     ),
-  //   );
-  //   archive.pipe(output);
-  //   archive.directory(
-  //     resolve(electronPackagerArtifactsDir, "Orange-darwin-x64/"),
-  //     false,
-  //   );
-  //   output.on("close", resolve_);
-  //   archive.on("error", err => {
-  //     throw err;
-  //   });
-  //   archive.finalize();
-  // });
+  console.log("Compressing Electron package...");
+  await new Promise(resolve_ => {
+    const archive = archiver("zip", { zlib: { level: 9 } });
+    const output = fs.createWriteStream(
+      `artifacts/electronPackager/${platformDefinition.archiveName}`,
+    );
+    archive.pipe(output);
+    archive.directory(
+      `artifacts/electronPackager/Orange-${platformDefinition.electronPackagerPlatform}-x64/`,
+      false,
+    );
+    output.on("close", resolve_);
+    archive.on("error", error => {
+      console.error(error);
+      core.setFailed(`Could not create a zip archive on ${os}`);
+    });
+    archive.finalize();
+  });
 
-  // console.log("Done!");
+  console.log(`Uploading artifact ${platformDefinition.archiveName}...`);
+  await artifactClient.uploadArtifact(
+    platformDefinition.archiveName,
+    [`artifacts/electronPackage/${platformDefinition.archiveName}`],
+    "artifacts/electronPackager",
+  );
 }
-
-// bluebird.try(buildPackage).catch(console.error);

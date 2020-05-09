91eceda25330e8b00de5cdfc14d636924017c1ab
M.K. Safi <msafi@msafi.com>
2020-05-09 11:25:22 -0700

    Ensure triggerRelease is on master (#45)
    * Ensure master
    
    * `cd` first

--- scripts/triggerRelease.js
+++ scripts/triggerRelease.js
@@ -1,31 +1,46 @@
 /* eslint-disable no-console */
-const shelljs = require("shelljs");
+const { echo, exit, exec, cd } = require("shelljs");
 const { resolve } = require("path");
 
 const [, , version] = process.argv;
 const semverRegExp = /^([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/;
 const repoUrl = "git@github.com:orange-org/orange.git";
 
+cd(resolve(__dirname, ".."));
+
 if (!version.match(semverRegExp)) {
-  shelljs.echo(`"${version}" is not a valid semantic version`);
-  shelljs.exit(1);
+  echo(`"${version}" is not a valid semantic version`);
+  exit(1);
 }
 
-shelljs.cd(resolve(__dirname, ".."));
+echo("Checking current branch");
+const currentBranch = exec("git rev-parse --abbrev-ref HEAD", {
+  silent: true,
+});
 
-shelljs.echo(`Creating git tag ${version}...`);
-if (shelljs.exec(`git tag ${version}`).code !== 0) {
-  shelljs.echo("Error: git tag failed");
-  shelljs.exit(1);
+if (currentBranch.code !== 0) {
+  echo("Error: `git rev-parse --abbrev-ref HEAD` failed");
+  exit(1);
 }
 
-shelljs.echo(`Pushing git tag ${version}...`);
-if (shelljs.exec(`git push ${repoUrl} ${version}`).code !== 0) {
-  shelljs.echo("Error: git push failed");
-  shelljs.exit(1);
+if (currentBranch.stdout !== "master") {
+  echo("Error: you must be on `master` to trigger a release");
+  exit(1);
 }
 
-shelljs.echo(
+echo(`Creating git tag ${version}...`);
+if (exec(`git tag ${version}`).code !== 0) {
+  echo("Error: git tag failed");
+  exit(1);
+}
+
+echo(`Pushing git tag ${version}...`);
+if (exec(`git push ${repoUrl} ${version}`).code !== 0) {
+  echo("Error: git push failed");
+  exit(1);
+}
+
+echo(
   `\n\n"${version}" has been successfully pushed to ${repoUrl}.\n` +
     "This should trigger a GitHub Actions draft release https://github.com/orange-org/orange/actions",
 );

23879336a099dad0637fa455c06488e46ef91a40
M.K. Safi <msafi@msafi.com>
2020-05-26 15:34:07 -0700

    Fix executable's name on Windows (take 2)

--- webpack/CopyBinsPlugin.ts
+++ webpack/CopyBinsPlugin.ts
@@ -6,8 +6,9 @@ const { platform, arch } = process;
 
 const rootDir = getRootDir();
 const rootDirSrc = `${rootDir}/src`;
+const btcd = platform === "win32" ? "btcd.exe" : "btcd";
 const btcdSrcPath = `${rootDirSrc}/bin/${platform}-${arch}/btcd`;
-const btcdDestinationPath = `${rootDir}/artifacts/webpack/bin/${platform}-${arch}/btcd`;
+const btcdDestinationPath = `${rootDir}/artifacts/webpack/bin/${platform}-${arch}/${btcd}`;
 
 export class CopyBinsPlugin {
   apply = (compiler: Webpack.Compiler) => {

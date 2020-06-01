65b6fbc8c84568851ac3165a4a5170440221a74d
M.K. Safi <msafi@msafi.com>
2020-05-31 23:39:44 -0700

    Compile UrlGuard.test.ts

--- src/main/WindowManager/MainWindow/UrlGuard/UrlGuard.test.ts
+++ src/main/WindowManager/MainWindow/UrlGuard/UrlGuard.test.ts
@@ -1,69 +1,64 @@
-// import { getIsDevelopment as getIsDevelopment_ } from "_m/getIsDevelopment";
-// import { Utils } from "_m/Utils";
-// import { UrlGuard } from "./UrlGuard";
+import { Utils } from "_m/common/Utils";
+import { UrlGuard } from "./UrlGuard";
 
-// const getIsDevelopment = getIsDevelopment_ as jest.Mock;
+describe("UrlGuard.isAllowed", () => {
+  describe("during development", () => {
+    beforeAll(() => {
+      // @ts-ignore
+      Utils.isDevelopment.mockImplementation(() => true);
+    });
 
-// jest.mock("_m/getIsDevelopment", () => ({
-//   getIsDevelopment: jest.fn(),
-// }));
+    it("allows requests to specific google domains to install extensions", () => {
+      expect(UrlGuard.isAllowed("https://clients2.googleusercontent.com")).toBe(
+        true,
+      );
+      expect(UrlGuard.isAllowed("https://clients2.google.com")).toBe(true);
+    });
 
-// describe("UrlGuard.isAllowed", () => {
-//   describe("during development", () => {
-//     beforeAll(() => {
-//       getIsDevelopment.mockImplementation(() => true);
-//     });
+    it("allows devtools", () => {
+      expect(UrlGuard.isAllowed("devtools://devtools/one")).toBe(true);
+    });
 
-//     it("allows requests to specific google domains to install extensions", () => {
-//       expect(UrlGuard.isAllowed("https://clients2.googleusercontent.com")).toBe(
-//         true,
-//       );
-//       expect(UrlGuard.isAllowed("https://clients2.google.com")).toBe(true);
-//     });
+    it("allows Chrome extensions", () => {
+      expect(UrlGuard.isAllowed("chrome-extension://redux-devtools")).toBe(
+        true,
+      );
+      expect(UrlGuard.isAllowed("blob:chrome-extension://hniebl")).toBe(true);
+    });
 
-//     it("allows devtools", () => {
-//       expect(UrlGuard.isAllowed("devtools://devtools/one")).toBe(true);
-//     });
+    it("allows localhost for something like Hot Module Replacement", () => {
+      expect(UrlGuard.isAllowed("http://localhost/some.json")).toBe(true);
+    });
 
-//     it("allows Chrome extensions", () => {
-//       expect(UrlGuard.isAllowed("chrome-extension://redux-devtools")).toBe(
-//         true,
-//       );
-//       expect(UrlGuard.isAllowed("blob:chrome-extension://hniebl")).toBe(true);
-//     });
+    it("allows files from the same app directory as Orange", () => {
+      expect(UrlGuard.isAllowed(`file://${Utils.getAppRoot()}/okay.json`)).toBe(
+        true,
+      );
+    });
 
-//     it("allows localhost for something like Hot Module Replacement", () => {
-//       expect(UrlGuard.isAllowed("http://localhost/some.json")).toBe(true);
-//     });
+    it("blocks everything else", () => {
+      expect(UrlGuard.isAllowed("http://google.com/some.json")).toBe(false);
+      expect(UrlGuard.isAllowed("ftp://google.com/some.json")).toBe(false);
+      expect(UrlGuard.isAllowed("blob://hello/some.json")).toBe(false);
+    });
+  });
 
-//     it("allows files from the same app directory as Orange", () => {
-//       expect(UrlGuard.isAllowed(`file://${Utils.getAppRoot()}/okay.json`)).toBe(
-//         true,
-//       );
-//     });
+  describe("during production", () => {
+    beforeAll(() => {
+      // @ts-ignore
+      Utils.isDevelopment.mockImplementation(() => false);
+    });
 
-//     it("blocks everything else", () => {
-//       expect(UrlGuard.isAllowed("http://google.com/some.json")).toBe(false);
-//       expect(UrlGuard.isAllowed("ftp://google.com/some.json")).toBe(false);
-//       expect(UrlGuard.isAllowed("blob://hello/some.json")).toBe(false);
-//     });
-//   });
+    it("allows files from the same app directory as Orange", () => {
+      expect(UrlGuard.isAllowed(`file://${Utils.getAppRoot()}/okay.json`)).toBe(
+        true,
+      );
+    });
 
-//   describe("during production", () => {
-//     beforeAll(() => {
-//       getIsDevelopment.mockImplementation(() => false);
-//     });
-
-//     it("allows files from the same app directory as Orange", () => {
-//       expect(UrlGuard.isAllowed(`file://${Utils.getAppRoot()}/okay.json`)).toBe(
-//         true,
-//       );
-//     });
-
-//     it("blocks everything else", () => {
-//       expect(UrlGuard.isAllowed("http://google.com/some.json")).toBe(false);
-//       expect(UrlGuard.isAllowed("ftp://google.com/some.json")).toBe(false);
-//       expect(UrlGuard.isAllowed("blob://hello/some.json")).toBe(false);
-//     });
-//   });
-// });
+    it("blocks everything else", () => {
+      expect(UrlGuard.isAllowed("http://google.com/some.json")).toBe(false);
+      expect(UrlGuard.isAllowed("ftp://google.com/some.json")).toBe(false);
+      expect(UrlGuard.isAllowed("blob://hello/some.json")).toBe(false);
+    });
+  });
+});

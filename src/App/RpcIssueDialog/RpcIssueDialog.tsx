710619c1074ebca043667b1598c60774452aee8a
M.K. Safi <msafi@msafi.com>
2020-07-26 15:12:22 -0700

    Delete more obsolete files

--- src/App/RpcIssueDialog/RpcIssueDialog.tsx
+++
@@ -1,7 +0,0 @@
-import { FeatureFlags } from "_r/FeatureFlags/FeatureFlags";
-import { AwaitBtcd } from "./AwaitBtcd/AwaitBtcd";
-import { FixBcoreConnection } from "./FixBcoreConnection/FixBcoreConnection";
-
-export const RpcIssueDialog = FeatureFlags.useBcore
-  ? FixBcoreConnection
-  : AwaitBtcd;

e2fabad6e457c85fd759540b652d1d2d763fc6d0
M.K. Safi <msafi@msafi.com>
2020-08-25 18:17:43 -0700

    Add validation to confirm secret page

--- src/App/Wallet/WalletCreate/WalletCreateConfirm/WalletCreateConfirm.tsx
+++ src/App/Wallet/WalletCreate/WalletCreateConfirm/WalletCreateConfirm.tsx
@@ -1,63 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Page } from "src/App/common/Page";
 import styles from "src/styles.css";
 import { cn } from "src/cn";
 import { Link } from "react-router-dom";
 import { FiChevronLeft } from "react-icons/fi";
 import { Icon } from "src/App/common/Icon";
+import { useFormik } from "formik";
 
-export const WalletCreateConfirm: React.FC<{ mnemonic: string }> = () => (
-  <Page title="Confirm Secret">
-    <p>
-      Enter your secret phrase below to confirm that you&apos;ve noted it down
-      correctly
-    </p>
+export const WalletCreateConfirm: React.FC<{ mnemonic: string }> = p => {
+  const formik = useFormik({
+    validateOnMount: true,
+    initialValues: {
+      enteredMnemonic: "",
+      iUnderstand: false,
+    },
+    validate: values => {
+      const errors = {
+        enteredMnemonic: "",
+        iUnderstand: "",
+      };
 
-    <textarea name="mnemonicTextarea" id="mnemonicTextarea" rows={5} />
+      if (values.enteredMnemonic !== p.mnemonic) {
+        errors.enteredMnemonic = "error";
+      }
 
-    <p>
-      Orange will not store your secret and will ask you again for it before you
-      can send money out.
-    </p>
+      if (values.iUnderstand !== true) {
+        errors.iUnderstand = "error";
+      }
 
-    <label htmlFor="orangeDoesNotStoreSecretPhrase" {...cn(styles.displayFlex)}>
-      <span>
-        <input
-          {...cn(styles.transformScale1_5)}
-          type="checkbox"
-          name="orangeDoesNotStoreSecretPhrase"
-          id="orangeDoesNotStoreSecretPhrase"
-        />
-      </span>
-      <span {...cn(styles.marginLeft3)}>
-        I understand that Orange does not store this secret phrase and that I
-        will need it to send money out.
-      </span>
-    </label>
+      if (Object.values(errors).every(errorValue => errorValue.length === 0)) {
+        return {};
+      }
 
-    <div {...cn(styles.marginTop10)} />
+      return errors;
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-    <div {...cn(styles.displayFlex, styles.alignItemsCenter)}>
-      <Link to="/wallet/create" {...cn(styles.flex1)}>
-        <button
-          {...cn(
-            styles.borderNone,
-            styles.displayFlex,
-            styles.alignItemsCenter,
-            styles.paddingLeft0,
-          )}
-          type="button"
-        >
-          <Icon IconType={FiChevronLeft} />
-          Go back
-        </button>
-      </Link>
+  return (
+    <Page title="Confirm Secret">
+      <p>
+        Enter your secret phrase below to confirm that you&apos;ve wrote it down
+        correctly
+      </p>
 
-      <Link to="/wallet">
-        <button type="button" disabled>
-          Open wallet
-        </button>
-      </Link>
-    </div>
-  </Page>
-);
+      <textarea
+        id="mnemonicTextarea"
+        rows={5}
+        {...formik.getFieldProps("enteredMnemonic")}
+      />
+
+      <p>
+        Orange will not store your secret but will ask you again for it before
+        you can send money out.
+      </p>
+
+      <label
+        htmlFor="orangeDoesNotStoreSecretPhrase"
+        {...cn(styles.displayFlex)}
+      >
+        <span>
+          <input
+            {...cn(styles.transformScale1_5)}
+            type="checkbox"
+            id="orangeDoesNotStoreSecretPhrase"
+            {...formik.getFieldProps("iUnderstand")}
+          />
+        </span>
+        <span {...cn(styles.marginLeft3)}>
+          I understand that Orange does not store this secret phrase but I will
+          need it to send money out.
+        </span>
+      </label>
+
+      <div {...cn(styles.marginTop10)} />
+
+      <div {...cn(styles.displayFlex, styles.alignItemsCenter)}>
+        <Link to="/wallet/create" {...cn(styles.flex1)}>
+          <button
+            {...cn(
+              styles.borderNone,
+              styles.displayFlex,
+              styles.alignItemsCenter,
+              styles.paddingLeft0,
+            )}
+            type="button"
+          >
+            <Icon IconType={FiChevronLeft} />
+            Go back
+          </button>
+        </Link>
+
+        <Link to="/wallet">
+          <button type="button" disabled={!formik.isValid}>
+            Open wallet
+          </button>
+        </Link>
+      </div>
+    </Page>
+  );
+};
